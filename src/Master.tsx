a52103fc0a63ea140b39b7a349aa2e087f2f9b35
WhiteKr <hanjisu2215g@gmail.com>
2021-04-14 08:32:08 +0900

    Changed function to class

---
+++ src/Master.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/Master.css';
+
+class Master extends React.Component {
+	render() {
+		return (
+			<div className="Master">
+			</div>
+		)
+	}
+}
+
+export default Master;
